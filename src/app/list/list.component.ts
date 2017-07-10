f0e92952e0f1dd5d6267b8a9d102ed12c123846a
최대은 <cde0701@gmail.com>
2017-07-10 17:32:21 +0900

    add continent list

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export class Continent{
+  constructor( id:number, name:string){
+  }
+}
+
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -11,5 +18,19 @@ export class ListComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  continent : Continent = [
+
+    {id: 1, name: 'Europe'},
+    {id: 2, name: 'Asia'},
+    {id: 3, name: 'Africa'},
+    {id: 4, name: 'North America'},
+    {id: 5, name: 'South America'},
+    {id: 6, name: 'Antarctica'},
+    {id: 7, name: 'Australia'},
+
+  ];
+
+
 
 }
